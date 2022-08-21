6fbfe3c01826d908aa488ec3c3be605326026ed2
Yimin Zhang <yiminzzz@qq.com>
2022-08-21 14:44:21 +1200

    rule functions updated

--- src/tidyfol/command/rule.py
+++ src/tidyfol/command/rule.py
@@ -8,16 +8,65 @@ def rule(args):
     print("new rules")
 
 
+def touch(sub_dir, name="*"):
+    with open(config.RULES_PATH) as f:
+        rules = json.load(f)
+
+    with open(config.RULES_PATH, "w") as f:
+        json.dump(rules, f)
+
+
+def mkdir(sub_dir):
+    with open(config.RULES_PATH) as f:
+        rules = json.load(f)
+
+    with open(config.RULES_PATH, "w") as f:
+        json.dump(rules, f)
+
+
+def mv(sub_dir):
+    with open(config.RULES_PATH) as f:
+        rules = json.load(f)
+
+    with open(config.RULES_PATH, "w") as f:
+        json.dump(rules, f)
+
+
+def rm(sub_dir):
+    with open(config.RULES_PATH) as f:
+        rules = json.load(f)
+
+    with open(config.RULES_PATH, "w") as f:
+        json.dump(rules, f)
+
+
 def default_rules(restore=False):
     # TODO: add more rules
     rules = [{
-        "Screen Shots": {
+        "is_dir": True,
+        "Description": "Screen Shots",
+        "Screen Shots": [{
+            "is_dir": False,
             "name": "Screen Shot*"
-        }
+        }]
     }, {
-        "Other": {
+        "is_dir": True,
+        "Description": "Source Code",
+        "Source Code": [{
+            "is_dir": True,
+            "Description": "Python Code",
+            "Python": [{
+                "is_dir": False,
+                "name": "*.py"
+            }]
+        }]
+    }, {
+        "is_dir": True,
+        "Description": "Other Files",
+        "Other": [{
+            "is_dir": False,
             "name": "*"
-        }
+        }]
     }]
 
     if restore or (not os.path.exists(config.RULES_PATH)):
--- src/tidyfol/config.py
+++ src/tidyfol/config.py
@@ -2,7 +2,7 @@ import os.path
 from platformdirs import *
 
 APP_NAME = "tidyfol"
-APP_AUTHOR = "Yimin"
+APP_AUTHOR = "Eminz"
 
 RULES_PATH = os.path.join(user_config_dir(APP_NAME, APP_AUTHOR), "rules.json")
 DB_PATH = os.path.join(user_data_dir(APP_NAME, APP_AUTHOR), "tidyfol.db")
